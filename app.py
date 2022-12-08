547a5bcafb181ae3ea9f30950af4af5a89221406
Kritsaleodas <100091650+Kritsaleodas@users.noreply.github.com>
2022-12-08 10:57:27 +0000

    uhh

--- app.py
+++ app.py
@@ -8,9 +8,7 @@ from datetime import date
 app = Flask(__name__)
 
 # Configure CS50 Library to use SQLite database
-uri = os.getenv("postgres://srvqnueqstkzbv:3064c19f6fa2577038f4c9ed72a1088cfcffbc3821b81185b6ab1a44c1141491@ec2-176-34-215-248.eu-west-1.compute.amazonaws.com:5432/d9vd6rcei525g")
-if uri.startswith("postgres://"):
-    uri = uri.replace("postgres://", "postgresql://")
+uri = "postgresql://srvqnueqstkzbv:3064c19f6fa2577038f4c9ed72a1088cfcffbc3821b81185b6ab1a44c1141491@ec2-176-34-215-248.eu-west-1.compute.amazonaws.com:5432/d9vd6rcei525g"
 db = SQL(uri)
 
 x = date.today().weekday()
--- help.py
+++ help.py
@@ -1,8 +1,5 @@
-
+import os
 from cs50 import SQL
 
-db = SQL("sqlite:///project.db")
-
-db.execute("DELETE FROM stats WHERE NOT week = 0;")
-x = db.execute("UPDATE stats SET tasks_done = 0, tasks_assigned = 0 WHERE week = 0;")
-
+uri = os.getenv("postgres://srvqnueqstkzbv:3064c19f6fa2577038f4c9ed72a1088cfcffbc3821b81185b6ab1a44c1141491@ec2-176-34-215-248.eu-west-1.compute.amazonaws.com:5432/d9vd6rcei525g")
+print(uri)
